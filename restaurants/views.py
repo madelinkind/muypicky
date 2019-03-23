5a140a3df20930531884ddea5fbb6010725ef5c9
madelinkind <madelinkind@gmail.com>
2019-03-23 18:06:27 -0400

    testing templates inheritance

--- restaurants/views.py
+++ restaurants/views.py
@@ -5,27 +5,56 @@ from django.shortcuts import render
 
 # Create your views here.
 # function based view
-def home_old(request):
-    html_var = 'f strings'
-    html_ = """<!DOCTYPE html>
-      <html lang="en">
-        <head>
-        </head>
-        <body>
-          <h1>Hello World!</h1>
-          <p>This is {} coming though</p>
-          </body>
-      </html>
-
-    """.format(
-      html_var
-    )
-
-    #f strings
-    return HttpResponse(html_)
+# def home_old(request):
+#     html_var = 'f strings'
+#     num = random.randint(0, 100000000)
+#     html_ = """<!DOCTYPE html>
+#       <html lang="en">
+#         <head>
+#         </head>
+#         <body>
+#           <h1>Hello World!</h1>
+#           <p>This is {} coming though</p>
+#           <p>This is random number {num}</p>
+#           </body>
+#       </html>
+#
+#     """.format(
+#       html_var
+#     )
+#
+#     #f strings
+#     return HttpResponse(html_)
 
 
 def home(request):
-    num = random.randint(0, 100000000)
-    return render(request, "base.html", {"html_var": True, "num": num})
+    num = None
+    some_list = [
+        random.randint(0, 100000000),
+        random.randint(0, 100000000),
+        random.randint(0, 100000000)
+    ]
+    condition_bool_item = True
+    if condition_bool_item:
+        num = random.randint(0, 100000000)
+    context = {
+        "some_list": some_list,
+        "num": num
+
+    }
+    return render(request, "home.html", context)
+
+
+def home2(request):
+    context = {
+    }
+    return render(request, "home2.html", context)
+
+
+def home3(request):
+    context = {
+    }
+    return render(request, "home3.html", context)
+
+
 
